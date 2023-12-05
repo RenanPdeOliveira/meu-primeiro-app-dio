09603ec2e975faf3c86e7d8835894a81484d9c60
renanpires919 <renanpires919mx@gmail.com>
2023-12-05 11:28:57 -0300

    languages implemented

--- app/src/main/java/br/com/igorbag/meuprimeiroappdio/MainActivity.kt
+++ app/src/main/java/br/com/igorbag/meuprimeiroappdio/MainActivity.kt
@@ -2,13 +2,15 @@ package br.com.igorbag.meuprimeiroappdio
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import br.com.igorbag.meuprimeiroappdio.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        //O desafio sera criar uma valor dentro do string.xml
-        // E trocar o texto do xml e tornar internacional (Ingles, Espanhol, etc...)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
